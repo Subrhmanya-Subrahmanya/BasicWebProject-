db621ea19a2727d65c4d4a720399dba00f8be1be
Subrahmanya-Subrahmanya <subrahmanya.kn19@gmail.com>
2025-04-12 23:46:53 +0530

    instrument deatils menu btn changed to select btn

--- Day3/instrument-tracker/src/app/components/instrument-buttons/instrument-buttons.component.ts
+++ Day3/instrument-tracker/src/app/components/instrument-buttons/instrument-buttons.component.ts
@@ -1,24 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
 import { InstrumentService } from '../../services/instrument.service';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-instrument-buttons',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatSelectModule, MatFormFieldModule, ScrollingModule],
   templateUrl: './instrument-buttons.component.html',
   styleUrl: './instrument-buttons.component.css'
 })
 export class InstrumentButtonsComponent {
   @Output() instrumentSelected = new EventEmitter<string>();
-  selectedInstrument: string | null = null;
+  public selectedInstrument: string | null = null;
+  @Input() instruments: any;
 
-  constructor(private instrumentService: InstrumentService) {
+  constructor(private instrumentService: InstrumentService,
+    private cdr: ChangeDetectorRef
+  ) {
     
   }
+  ngOnInit() {
+   // this.cdr.detectChanges();
+  }
+  selectInstrument(id: string | null) {
 
-  selectInstrument(id: string) {
-    this.selectedInstrument = id;
-    this.instrumentSelected.emit(id);
+    if (id !== null) {
+      this.instrumentSelected.emit(id);
+    }
     
   }
 
